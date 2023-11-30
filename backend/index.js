c8ff24e80d76dbcb267c9537df05aee027fcdec3
Jitesh Tripathi <jiteshtripathi16480@gmail.com>
2023-12-01 01:36:55 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 // app.use(cors());
 app.use(
     cors({
-        origin: 'http://localhost:5173',
+        origin: 'https://bookstorejt.vercel.app',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type'],
     })
